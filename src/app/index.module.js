46d53fbbce2d11a88d8986f703a8ef1733f4d2ae
Marek Skrajnowski <m.skrajnowski@gmail.com>
2015-12-13 16:06:28 +0100

    scaffold cleanup

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,10 +4,6 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
 angular.module('acodemySpotifyFork', ['ui.router', 'toastr'])
   .constant('malarkey', malarkey)
@@ -15,8 +11,4 @@ angular.module('acodemySpotifyFork', ['ui.router', 'toastr'])
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
-  .controller('MainController', MainController)
-  .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective);
+  .controller('MainController', MainController);
